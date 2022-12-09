67e22309936230083b6ecaaa013ccb37dc282cf4
Klazen108 <cmurphy1337@live.com>
2022-12-09 03:14:46 +0000

    day 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("hello world");
+
+var fs = require('fs');
+var array = fs.readFileSync('day1.input.txt').toString().split("\n");
+
+const result = array.reduce(
+    (accumulator, currentValue) => {
+        if (currentValue === "") {
+            accumulator.push(0);
+        } else {
+            accumulator[accumulator.length-1]+= +currentValue;
+        }
+        return accumulator;
+    },
+    []
+);
+
+const answer = result.reduce(
+    (acc,val,idx) => {
+        if (val>acc.val) return {idx,val};
+        return acc;
+    },
+    {idx:-1,val:0}
+);
+
+console.log(answer);
